64be488b6500ff19ee0775116a917ca71d1e54f9
Nithesh <nitheshwaran003@gmail.com>
2025-08-07 17:24:20 +0530

    api update

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, World! This is a Flask app."
 
+@app.route('/hello')
+def hello():
+    return "Hello, I'm here .. !"
+
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5001)
